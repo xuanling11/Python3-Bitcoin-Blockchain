3d579b3cda83db80c43dc18086861805d36d3f55
xuanling11 <57745186+xuanling11@users.noreply.github.com>
2022-08-18 18:05:22 -0700

    Create transactions.py

---
+++ transactions.py
@@ -0,0 +1,52 @@
+transaction1 = {
+  'amount': '30',
+  'sender': 'Alice',
+  'receiver': 'Bob'}
+transaction2 = { 
+  'amount': '200',
+  'sender': 'Bob',
+  'receiver': 'Alice'}
+transaction3 = { 
+  'amount': '300',
+  'sender': 'Alice',
+  'receiver': 'Timothy' }
+transaction4 = { 
+  'amount': '300',
+  'sender': 'Rodrigo',
+  'receiver': 'Thomas' }
+transaction5 = { 
+  'amount': '200',
+  'sender': 'Timothy',
+  'receiver': 'Thomas' }
+transaction6 = { 
+  'amount': '400',
+  'sender': 'Tiffany',
+  'receiver': 'Xavier' }
+
+mempool = [transaction1, transaction2, transaction3, transaction4, transaction5, transaction6]
+
+# add your code below
+
+#1 add a new transaction
+my_transaction = { 
+  'amount': '100',
+  'sender': 'George',
+  'receiver': 'Alex' }
+
+#2 add new transaction to the existing one
+mempool.append(my_transaction)
+
+#test print
+#print(mempool)
+
+#3 create a new list of addtional three new transactions
+
+block_transactions = mempool[0:3]
+print(block_transactions)
+
+
+
+
+
+
+
