2580b7865054e00a7839f4fb17c79d621736c578
xuanling11 <57745186+xuanling11@users.noreply.github.com>
2022-08-18 23:55:18 -0700

    Update test_complete_blockchain.py

--- test_complete_blockchain.py
+++ test_complete_blockchain.py
@@ -1,9 +1,9 @@
 from blockchain import Blockchain
 
 block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
-block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
-block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
-fake_transactions = {"sender": "Bob", "receiver":"Cole, Alice", "amount":"25"}
+block_two_transactions = {"sender": "Bob", "receiver":"Alice", "amount":"25"}
+block_three_transactions = {"sender":"Alice", "receiver":"Bob", "amount":"35"}
+fake_transactions = {"sender": "Bob", "receiver":"Alice", "amount":"25"}
 
 local_blockchain = Blockchain()
 #creates a Genesis Block
