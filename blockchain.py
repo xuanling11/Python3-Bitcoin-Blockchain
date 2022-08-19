3f07873e5510d9442d566259fac8abe99d0848a1
xuanling11 <57745186+xuanling11@users.noreply.github.com>
2022-08-18 19:09:15 -0700

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,15 @@
+#imports the Block class from block.py
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+      self.chain = []
+      self.all_transactions = []
+      self.genesis_block() #start with genesis block
+
+    #initiate with genesis block
+    def genesis_block(self):
+      Block(self.all_transactions, previous_hash = 0)
+      self.chain.append(Block)
+
 
