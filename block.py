d2efc9fadd31a27aeef1942e996b447038718c4c
xuanling11 <57745186+xuanling11@users.noreply.github.com>
2022-08-18 21:22:19 -0700

    Update block.py

--- block.py
+++ block.py
@@ -1,14 +1,21 @@
-# import datetime library
-from datetime import datetime
-
-# print current date and time
-print(datetime.now())
+import datetime
+from hashlib import sha256
 
 class Block:
-# default constructor for block class
-  def __init__(self, transactions, previous_hash, nonce = 0):
+  def __init__(self, transactions, previous_hash):
+    self.timestamp = datetime.datetime.now()
     self.transactions = transactions
     self.previous_hash = previous_hash
-    self.nonce = nonce
-    self.timestamp = datetime.now()
+    self.hash = self.generate_hash()
     
+  def print_block(self):
+    # prints block contents
+    print("timestamp:", self.timestamp)
+    print("transactions:", self.transactions)
+    print("current hash:", self.generate_hash())
+    
+  def generate_hash(self):
+    block_contents = str(self.timestamp) + str(self.transactions) + str(self.previous_hash)
+    block_hash = sha256(block_contents.encode())
+    return block_hash.hexdigest()
+  
