78657c3a478c218b9c7e6119814a017192a42428
xuanling11 <57745186+xuanling11@users.noreply.github.com>
2022-08-18 21:41:57 -0700

    Update block.py

--- block.py
+++ block.py
@@ -1,21 +1,42 @@
-import datetime
-from hashlib import sha256
+#imports the Block class from block.py
+from block import Block
 
-class Block:
-  def __init__(self, transactions, previous_hash):
-    self.timestamp = datetime.datetime.now()
-    self.transactions = transactions
-    self.previous_hash = previous_hash
-    self.hash = self.generate_hash()
-    
-  def print_block(self):
-    # prints block contents
-    print("timestamp:", self.timestamp)
-    print("transactions:", self.transactions)
-    print("current hash:", self.generate_hash())
-    
-  def generate_hash(self):
-    block_contents = str(self.timestamp) + str(self.transactions) + str(self.previous_hash)
-    block_hash = sha256(block_contents.encode())
-    return block_hash.hexdigest()
+class Blockchain:
+  def __init__(self):
+    self.chain = []
+    self.all_transactions = []
+    self.genesis_block()
+
+  def genesis_block(self):
+    transactions = {}
+    genesis_block = Block(transactions, "0")
+    self.chain.append(genesis_block)
+    return self.chain
+
+  # prints contents of blockchain
+  def print_blocks(self):
+    for i in range(len(self.chain)):
+      current_block = self.chain[i]
+      print("Block {} {}".format(i, current_block))
+      current_block.print_contents()    
   
+  # add block to blockchain `chain`
+  def add_block(self, transactions):
+    previous_block_hash = self.chain[len(self.chain)-1].hash
+    new_block = Block(transactions, previous_block_hash)
+    self.chain.append(new_block)
+
+  def validate_chain(self):
+    # loop starts with 1
+    for i in range(1, len(self.chain)):
+      # validator guess hash value
+      current = self.chain[i]
+      previous = self.chain[i-1]
+      # check if the validate hash is matching generate hash from the blockchain
+      if (current.hash != current.generate_hash()):
+        return False
+      elif (previous.hash != previous.generate_hash()):
+        return False
+        #only and if only both hash generate from the validator matches the blockchain generated hash, the block is valid and can be posted on the blockchain
+      else:
+        return True
