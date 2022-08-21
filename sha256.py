a64a9368713de95add9f9d41957f2d94f789297e
xuanling11 <57745186+xuanling11@users.noreply.github.com>
2022-08-20 21:45:18 -0700

    Update sha256.py

--- sha256.py
+++ sha256.py
@@ -5,11 +5,10 @@ from hashlib import sha256
 text = "I am excited to learn about blockchain"
 text1 = "I am excited to learn about blockchain!"
 
-# print result
+# encode the text
 hash_result = sha256(text.encode())
 hash_result1 = sha256(text1.encode())
 
+# print result
 print(hash_result.hexdigest())
 print(hash_result1.hexdigest())
-
-
